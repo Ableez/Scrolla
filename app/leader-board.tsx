4b12130f8a26039d3f96675b9235472dfb7b49f6
Ableez <djayableez@gmail.com>
2025-01-08 08:28:29 +0100

    leaderboard screen scaffold

---
+++ app/leader-board.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+
+const Leaderboard = () => {
+  return <View>{/* <Text>Leaderboard</Text> */}</View>;
+};
+
+export default Leaderboard;
+
+const styles = StyleSheet.create({});
